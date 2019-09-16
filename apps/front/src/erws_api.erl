270f1f76b7238c9786c2b833ac64b9c86f23f02c
me <me@me.home>
2019-09-16 16:03:12 +0300

    fix typo

--- apps/front/src/erws_api.erl
+++ apps/front/src/erws_api.erl
@@ -239,7 +239,7 @@ process([<<"callback">>, UserId], _, Body, Req, State )->
         List ->
                 lists:foreach(fun(ChatState)->
                      ChatState#chat_state.pid ! {deal_info, Body}
-                    end, List ),
+                    end, List )
      end
 ;
 process([<<"tasks">>, <<"mysecretkey2">>], _, _Body, Req, State)->
